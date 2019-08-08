bd099268c9a51470c4d17007720deee5b6e69564
Philip Ford <paf1687@gmail.com>
2019-08-08 15:20:18 -0400

    First commit

---
+++ src/test/java/com/giantmachines/biblio/controller/BookControllerIT.java
@@ -0,0 +1,16 @@
+package com.giantmachines.biblio.controller;
+
+import com.giantmachines.biblio.Application;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("Test")
+@AutoConfigureMockMvc
+public class BookControllerIT {
+
+}
