58174d35902ba1959c12e6c1b7be148537b499a2
Philip Ford <paf1687@gmail.com>
2019-09-22 11:26:26 -0400

    Obsolete import

--- src/main/java/com/giantmachines/biblio/dao/ReviewRepository.java
+++ src/main/java/com/giantmachines/biblio/dao/ReviewRepository.java
@@ -1,7 +1,6 @@
 package com.giantmachines.biblio.dao;
 
 import com.giantmachines.biblio.model.Review;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
